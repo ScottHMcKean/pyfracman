adc3a53b20a865b1445975f9431e0c54ceb55f59
Scott McKean <scott.mckean@ucalgary.ca>
2022-02-04 15:10:20 -0700

    Initial commit w/ setup

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name='pyfracman',
+    version='0.1',
+    description='A Python toolkit for FracMan',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Scott McKean',
+    author_email='scott.mckean@ucalgary.ca',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    keywords=['discrete fracture network', 'fracman', 'geoscience'],
+    package_dir={'': 'pyfracman'},
+    packages=find_packages(where='pyfracman'),
+    python_requires='>=3.6, <4',
+    install_requires=[
+        'pandas',
+        'numpy'
+        ],
+
+    # Not a bad idea
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+
+    # Will be added during development
+    # package_data={
+    #     'sample': ['package_data.dat'],
+    # }, 
+
+    project_urls={
+        'Fracman': 'https://www.golder.com/fracman/',
+    }
+)
