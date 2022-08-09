3a83ba4132ebb93b914cee95794db1529dcc167a
Scott McKean <scott.mckean@ucalgary.ca>
2022-08-09 10:12:05 -0600

    add f2d reader

--- pyfracman/data.py
+++ pyfracman/data.py
@@ -30,3 +30,39 @@ def read_ors(filename: str) -> pd.DataFrame:
     df = pd.read_csv(filename, delim_whitespace=True)
     df.columns = clean_columns(df.columns)
     return df
+
+def read_f2d_trace_file(filepath: str) -> pd.DataFrame:
+    """Read an f2d file to get trace information, grouping by TraceID
+    Takes the average of the lengths
+    returns the length, strike and dip only for now
+
+    Args:
+        filepath (str): filepath
+
+    Returns:
+        pd.DataFrame: length, strike, and dip per trace
+    """
+    columns = (
+        pd.read_csv(str(filepath), skiprows=1, nrows=1, header=None)
+        .iloc[0, 0]
+        .replace("#", "")
+        .replace("\t", "  ")
+        .split()
+    )
+    rows = pd.read_csv(filepath, skiprows=[0, 1], header=None)
+    fault_info = rows[0].str.replace("\t", " ").str.split(expand=True)
+    fault_info.columns = columns
+
+    fault_info = fault_info.apply(pd.to_numeric, errors="coerce")
+    fault_info = fault_info[fault_info["len[m]"] > 0]
+
+    if "Seg_Strike[deg]" not in columns:
+        fault_info["Seg_Strike[deg]"] = fault_info["Seg_Trend[deg]"] - 90
+
+    fault_info = (
+        fault_info.groupby("TraceID")
+        .mean()[["totlen[m]", "Seg_Strike[deg]", "Seg_VerticalDev[deg]"]]
+        .reset_index(drop=True)
+    )
+    fault_info.columns = ["length", "strike", "dip"]
+    return fault_info
